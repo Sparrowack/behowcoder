3fb125a256d887e2d561d5b2abdf705aa50fb0f7
Sparrowack <37346702+Sparrowack@users.noreply.github.com>
2018-03-13 22:10:29 +0300

    changing
    hange

--- printList.js
+++ printList.js
@@ -41,17 +41,4 @@ function printReverseListRec(list)
   console.log(list.value);
 }
 
-var list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null
-      }
-    }
-  }
-};
-printReverseList(list);
+
